73af867dd97e130bfa9e027b37c5714e0d668e89
infinitepure <matt.j.cheah@gmail.com>
2017-11-13 23:14:46 +1100

    Register models to the admin site

--- bets/admin.py
+++ bets/admin.py
@@ -1,3 +1,7 @@
+from .models import Bet, Bettor, Taker
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Bet)
+admin.site.register(Bettor)
+admin.site.register(Taker)
