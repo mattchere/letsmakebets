d34ffbc98022250eb9352ccddb63a5f25a92ab6a
infinitepure <matt.j.cheah@gmail.com>
2017-11-15 00:26:23 +1100

    Add navigation bar

--- letsmakebets/urls.py
+++ letsmakebets/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^bets/', include('bets.urls')),
     url(r'^$', RedirectView.as_view(url='/bets/', permanent=True)),
+    url(r'^', include('django.contrib.auth.urls')),
 ]
